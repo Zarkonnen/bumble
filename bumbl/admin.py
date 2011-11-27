dc5b13d9d59694568b0d21f3d4ffd28ede963481
Rachel Knowler <daisywheel22@gmail.com>
2011-11-27 16:47:02 +0100

    Implemented Entry model and way of querying by path.

--- bumbl/admin.py
+++ bumbl/admin.py
@@ -1,7 +1,4 @@
-from bumble.bumbl.models import Page, Update, Link, PictureEntry
+from bumble.bumbl.models import Entry
 from django.contrib import admin
 
-admin.site.register(Page)
-admin.site.register(Update)
-admin.site.register(Link)
-admin.site.register(PictureEntry)
+admin.site.register(Entry)
--- bumbl/models.py
+++ bumbl/models.py
@@ -2,30 +2,26 @@ from django.db import models
 
 class Entry(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    class Meta:
-        abstract = True
-        
-class Page(Entry):
     title = models.CharField(max_length=1000)
-    content = models.TextField()
+    slug = models.SlugField(blank=True)
+    css = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    parent = models.ForeignKey("self", blank=True, null=True, related_name="children")
+    path = models.TextField(blank=True)
+    def save(self, *args, **kwargs):
+        self.path = self.calculate_path()
+        super(Entry, self).save(*args, **kwargs)
+        for c in self.children.all():
+            c.save()
+    def calculate_path(self):
+        l = [self.slug]
+        e = self.parent
+        while e:
+            l.append(e.slug)
+            e = e.parent
+        return "/".join(l[::-1])
     def __unicode__(self):
         return self.title
-    
-class Update(Entry):
-    title = models.CharField(max_length=1000)
-    page = models.ForeignKey(Page)
-    def __unicode__(self):
-        return self.title
-
-class Link(Entry):
-    url = models.URLField('URL', max_length=1000)
-    def __unicode__(self):
-        return self.url
-    
-class PictureEntry(Entry):
-    name = models.CharField(max_length=1000)
-    def __unicode__(self):
-        return self.name
     class Meta:
-        verbose_name_plural = "picture entries"
+        verbose_name_plural = "entries"
     
