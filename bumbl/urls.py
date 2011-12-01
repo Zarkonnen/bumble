e1f53dc34b8186b307f5504e6725dc1415442faa
Rachel Knowler <daisywheel22@gmail.com>
2011-12-01 21:50:09 +0100

    Implemented displaying of entries, basic version.

--- bumbl/urls.py
+++ bumbl/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('bumble.bumbl.views',
     
-    url(r'^$', 'frontpage')
+    url(r'^(.*)$', 'entry')
 )
--- bumbl/views.py
+++ bumbl/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render_to_response
+from bumble.bumbl.models import Entry
 
-def frontpage(request):
-    return render_to_response('base.html', {})
+def entry(request, path):
+    if path.endswith('/'):
+        path = path[:-1]
+    if path != '':
+        path = '/' + path
+    return render_to_response('base.html', {'entry':Entry.objects.get(path=path), 'descendents':Entry.objects.filter(path__startswith=path+'/')})
