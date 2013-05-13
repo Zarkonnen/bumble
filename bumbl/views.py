1dca29146c36f320807c9a2ea80c8c662796d37d
bellisk <daisywheel22@gmail.com>
2013-05-13 18:20:37 +0200

    Added comment form on webpage.

--- bumbl/views.py
+++ bumbl/views.py
@@ -3,12 +3,14 @@ from bumble.bumbl.models import Entry, Tag
 from django.utils.feedgenerator import Atom1Feed
 from django.core.urlresolvers import reverse
 from bumble.bumbl.templatetags.tags import urlify_path
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from bumble.bumbl.settings import PAGINATION
 import json
 from django.conf import settings
 from django.utils.html import escape
 from bumble.bumbl.templatetags.tags import filepaths, md
+from bumble.bumbl.forms import CommentForm
+from django.core.context_processors import csrf
 
 def tag_filter(tags, objects):
     if len(tags) == 0:
@@ -42,7 +44,20 @@ def entry(request, path):
     if "/tag/" in path:
         entry_path, tags = path.split("/tag/")
         return render_to_response('tag.html', {'media':settings.MEDIA_URL, 'entry':get_object_or_404(Entry, path=entry_path), 'tags':tags.split("+"), "entries":get_tag_entries(tags.split("+"), entry_path)[0:PAGINATION], 'pagination_url':reverse("bumble.bumbl.views.page", args=[578329023, urlify_path(path)])})
-    return render_to_response('entry.html', {'media':settings.MEDIA_URL, 'entry':get_object_or_404(Entry, path=path), 'descendents':Entry.objects.filter(path__startswith=path+'/').order_by("-created")[0:PAGINATION], 'pagination_url':reverse("bumble.bumbl.views.page", args=[578329023, urlify_path(path)])})
+    e = get_object_or_404(Entry, path=path)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.entry = e
+            new_comment.ip = request.META['REMOTE_ADDR']
+            new_comment.save()
+            return HttpResponseRedirect(request.path)
+    else:
+        form = CommentForm()
+    c = {'media':settings.MEDIA_URL, 'entry':e, 'commentForm': form, 'descendents':Entry.objects.filter(path__startswith=path+'/').order_by("-created")[0:PAGINATION], 'pagination_url':reverse("bumble.bumbl.views.page", args=[578329023, urlify_path(path)])}
+    c.update(csrf(request))
+    return render_to_response('entry.html', c)
 
 def page(request, from_index, path):
     entries = []
@@ -57,3 +72,5 @@ def page(request, from_index, path):
         entries = Entry.objects.filter(path__startswith=path+'/').order_by("-created")
     entries = entries[int(from_index)*PAGINATION:int(from_index)*PAGINATION + PAGINATION]
     return HttpResponse(json.dumps([{"title": escape(e.title), "created": e.created, "description": md(filepaths(e.lead)), "link": reverse("bumble.bumbl.views.entry", args=[urlify_path(e.path)])} for e in entries]))
+
+        
