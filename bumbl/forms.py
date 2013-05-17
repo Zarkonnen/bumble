f83a1ab1bc258cb6b822f7c2ccf1a0bf328452d8
bellisk <daisywheel22@gmail.com>
2013-05-17 09:12:44 +0200

    Changed comment text input to textarea (bigger box).

--- bumbl/forms.py
+++ bumbl/forms.py
@@ -3,6 +3,8 @@ from django.forms import ModelForm
 from bumble.bumbl.models import Comment
 
 class CommentForm(ModelForm):
+    text = forms.CharField(max_length=5000, widget=forms.Textarea)
+
     class Meta:
         model = Comment
         fields = ["commenter", "email", "text"]
