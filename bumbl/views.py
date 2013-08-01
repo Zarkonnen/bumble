f2da2d3a9d57dd6ab9fd0fb099bd868ef2014675
bellisk <daisywheel22@gmail.com>
2013-08-01 16:49:48 +0200

    Using mail_admins instead of send_mail.

--- bumbl/views.py
+++ bumbl/views.py
@@ -12,7 +12,7 @@ from bumble.bumbl.forms import CommentForm
 from django.core.context_processors import csrf
 import requests
 from django.utils import formats
-from django.core.mail import send_mail
+from django.core.mail import send_mail, mail_admins
 
 def normalize_path(path):
     if path.endswith('/'):
@@ -78,7 +78,7 @@ def entry(request, path):
                 new_comment.entry = e
                 new_comment.ip = request.META['REMOTE_ADDR']
                 new_comment.save()
-                send_mail("New comment by " + new_comment.commenter, "<html><body><a href=\"" + entry_url(path) + "\">" + new_comment.commenter + "</a><br>" + new_comment.text + "</body></html>", settings.DEFAULT_FROM_EMAIL, [a[1] for a in settings.ADMINS])
+                mail_admins("New comment by " + new_comment.commenter, new_comment.text, html_message="<html><body><a href=\"" + entry_url(path) + "\">" + new_comment.commenter + "</a><br>" + new_comment.text + "</body></html>")
                 return HttpResponseRedirect(request.path)
     else:
         form = CommentForm()
