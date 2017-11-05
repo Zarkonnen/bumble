fca254b274013f4d6e51544466002a9272284db7
bellisk <daisywheel22@gmail.com>
2017-11-05 19:23:44 +0100

    Add placeholders to comment fields.
    Also commit the tags templatetag thing which we apparently missed earlier.

--- bumbl/forms.py
+++ bumbl/forms.py
@@ -3,9 +3,12 @@ from django.forms import ModelForm
 from bumble.bumbl.models import Comment
 
 class CommentForm(ModelForm):
-    text = forms.CharField(max_length=5000, widget=forms.Textarea)
-
     class Meta:
         model = Comment
         fields = ["commenter", "email", "text"]
+        widgets = {
+            'commenter': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'text': forms.Textarea(attrs={'placeholder': 'Comment'})
+        }
 
